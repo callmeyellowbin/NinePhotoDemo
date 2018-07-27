25b433657f845478166920091302e11ff43c461a
黄洪彬 <huanghongbin@corp.netease.com>
2018-07-27 09:40:10 +0800

    Initial Commit

---
+++ NinePhotoDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NinePhotoDemo
+//
+//  Created by 黄洪彬 on 2018/7/27.
+//  Copyright © 2018年 黄洪彬. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NinePhotoDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NinePhotoDemo
+//
+//  Created by 黄洪彬 on 2018/7/27.
+//  Copyright © 2018年 黄洪彬. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
